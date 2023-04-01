4ec6b8065eecf0423ef3d92cbecd7e3098020b60
nghiapham27 <nghiaph27@gmail.com>
2023-04-01 12:39:39 +0700

    updated favicon

--- src/components/UI/Brand.js
+++ src/components/UI/Brand.js
@@ -2,7 +2,10 @@ import { Link } from 'react-router-dom';
 
 const Brand = () => {
   return (
-    <Link className="text-4xl lg:text-5xl font-extrabold font-italianno shrink-0">
+    <Link
+      to={'/'}
+      className="text-4xl lg:text-5xl font-extrabold font-italianno shrink-0"
+    >
       <span className="text-transparent bg-clip-text bg-gradient-to-br from-amber-500 to-yellow-400 pr-2 ">
         FurniStore
       </span>
